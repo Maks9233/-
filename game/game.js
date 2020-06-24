f904564ca04898efab26ea7ce05efbceb3414e02
Maks9233 <60987243+Maks9233@users.noreply.github.com>
2020-06-24 16:17:23 +0300

    Update game.js

--- game/game.js
+++ game/game.js
@@ -53,8 +53,7 @@ function itemHandler(player, item) {
   item.kill();
   if (item.key === 'coin') {
      currentScore = currentScore + 10;
-  } else if (item.key === 'poison') {
-     currentScore = currentScore - 25;
+
   } else if (item.key === 'star') {
      currentScore = currentScore + 25;
   }
